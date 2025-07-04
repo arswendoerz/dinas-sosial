02039ff9e32f5e03d8dc920b4e17acd4b77f5624
OntongGhebi <ghebiarmando@gmail.com>
2025-07-04 15:25:22 +0700

    endpoint auth done

---
+++ back/middleware/verifyToken.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized - no token provided",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        message: "Unthorized - invalid token",
+      });
+    }
+
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Token Expired",
+      });
+    }
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid Token",
+      });
+    }
+    console.error("Error in verifyToken", error);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
--- back/route/auth.js
+++ back/route/auth.js
@@ -1,9 +1,11 @@
 // routes/auth.js
 import express from "express";
+import { register, login, logout } from "../controller/authController.js";
+
 const router = express.Router();
 
-router.post("/login", (req, res) => {
-  res.send("Login endpoint");
-});
+router.post("/login", login);
+router.post("/register", register);
+router.post("/logout", logout);
 
 export default router;
