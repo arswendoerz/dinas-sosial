d2d469954945d4e211dfbd93c1d526ab38f95f95
arswendoerz <arswendoerza00@gmail.com>
2025-07-29 08:17:12 +0700

    update plugin react

--- fe-dinas-sosial/vite.config.js
+++ fe-dinas-sosial/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
