e39ce2db45111e5889244c5f5b76153e39f75b1a
arswendoerz <arswendoerza00@gmail.com>
2025-07-31 10:37:17 +0700

    update cors url vercel

--- back/index.js
+++ back/index.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://dinas-sosial-lyart.vercel.app"
+      "https://dinas-sosial-nu.vercel.app"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
